bfed8d91b761e9fbcdfd6e324f551a5986318f7f
Poonyaporn Mahasupachai <poonyaporn.7353@gmail.com>
2025-11-27 15:54:21 -0500

    fix page style

--- _next/static/94FvgGfxZ_LgQQDRH1n5d/_buildManifest.js
+++ _next/static/94FvgGfxZ_LgQQDRH1n5d/_buildManifest.js
@@ -3,8 +3,8 @@ self.__BUILD_MANIFEST = {
     "afterFiles": [],
     "beforeFiles": [
       {
-        "source": "/portfolio//_next/:path+",
-        "destination": "/portfolio/_next/:path+"
+        "source": "/.//_next/:path+",
+        "destination": "/_next/:path+"
       }
     ],
     "fallback": []
