f8a215bd1e5b6fec46c5ead65e874436fa160957
Poonyaporn Mahasupachai <poonyaporn.7353@gmail.com>
2025-11-27 23:25:47 -0500

    fix page style

--- _next/static/JKOZe96vfijstCaKo7Gnm/_buildManifest.js
+++ _next/static/JKOZe96vfijstCaKo7Gnm/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/./mmpoonya.github.io//_next/:path+",
-        "destination": "/mmpoonya.github.io/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
